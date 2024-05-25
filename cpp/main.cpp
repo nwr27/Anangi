fe2364aac1022958df2d944e675ed7efac0287af
Nana Wartana <nanawartana27@gmail.com>
2024-05-25 18:21:45 +0700

    cpp

---
+++ cpp/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Halo, Mr. Nana Wartana" << endl;
+  return 0;
+}
+
+/*Run with these commands*/
+// g++ cpp / main.cpp - o cpp / maung
+// cpp / maung
